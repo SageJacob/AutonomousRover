059f2aca7c6b487a21537e644071606e143afbea
Charles <charlesmccampbellhill@gmail.com>
2022-01-31 08:56:07 -0800

    added adiona_description folder

---
+++ adiona_description/build/adiona_description/CMakeFiles/adiona_description_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/adiona_description_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/adiona_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
