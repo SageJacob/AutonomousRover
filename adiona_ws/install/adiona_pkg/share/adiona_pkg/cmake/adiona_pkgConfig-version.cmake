bc8fb54c178769091679c7e8015657c1a9d823ce
burke-hill <77991004+burke-hill@users.noreply.github.com>
2022-03-05 13:10:58 -0500

    Delete adiona_ws directory

--- adiona_ws/install/adiona_pkg/share/adiona_pkg/cmake/adiona_pkgConfig-version.cmake
+++
@@ -1,1 +0,0 @@
-/home/ubuntu/Desktop/AutonomousRover/adiona_ws/build/adiona_pkg/ament_cmake_core/adiona_pkgConfig-version.cmake
--- adiona_ws/install/adiona_pkg/share/adiona_pkg/local_setup.bash
+++
@@ -1,1 +0,0 @@
-/home/ubuntu/Desktop/AutonomousRover/adiona_ws/build/adiona_pkg/ament_cmake_environment_hooks/local_setup.bash
