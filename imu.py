67eb36d68a20d6fd18d5af980e2cfa7283a625e3
Jacob Sage <46393418+SageJacob@users.noreply.github.com>
2022-04-22 12:53:12 -0400

    Update imu.py

--- imu.py
+++ imu.py
@@ -4,7 +4,11 @@ import cv2
 import depthai as dai
 import time
 import math
-
+from scipy.spatial.transform import Rotation as R
+START = None
+FLAG = 0
+curr = None
+prev = None
 def setup():
     pipeline = dai.Pipeline()
     imu = pipeline.create(dai.node.IMU)
@@ -15,6 +19,10 @@ def setup():
     imu.enableIMUSensor(dai.IMUSensor.ACCELEROMETER_RAW, 500)
     # enable GYROSCOPE_RAW at 400 hz rate
     imu.enableIMUSensor(dai.IMUSensor.GYROSCOPE_RAW, 400)
+    # enable MAGNETOMETER_RAW at 400 hz rate
+    imu.enableIMUSensor(dai.IMUSensor.MAGNETOMETER_RAW, 400)
+    # enable ROTATION_VECTOR at 400 hz rate
+    imu.enableIMUSensor(dai.IMUSensor.ROTATION_VECTOR, 400)
     # it's recommended to set both setBatchReportThreshold and setMaxBatchReports to 20 when integrating in a pipeline with a lot of input/output connections
     # above this threshold packets will be sent in batch of X, if the host is not blocked and USB bandwidth is available
     imu.setBatchReportThreshold(1)
@@ -30,7 +38,6 @@ def setup():
 # Pipeline is defined, now we can connect to the device
 with dai.Device(setup()) as device:
 
-
     # Output queue for imu bulk packets
     imuQueue = device.getOutputQueue(name="imu", maxSize=50, blocking=False)
     while True:
@@ -39,12 +46,30 @@ with dai.Device(setup()) as device:
         
         for imuPacket in imuPackets:
             acceleroValues = imuPacket.acceleroMeter
-            gyroValues = imuPacket.gyroscope            
+            gyroValues = imuPacket.gyroscope
+            rVvalues = imuPacket.rotationVector        
+            r = R.from_quat([rVvalues.real, rVvalues.i, rVvalues.j, rVvalues.k]) 
 
             imuF = "{:.06f}"
+            #print(f"Quaternion: i: {imuF.format(rVvalues.i)} j: {imuF.format(rVvalues.j)} k: {imuF.format(rVvalues.k)} real: {imuF.format(rVvalues.real)}")
+            rotation = r.as_euler('zyz', degrees=True)
+            if FLAG == 0:
+                START = rotation[2] + 180
+                FLAG = 1
+                continue
+            raw = rotation[2] + 180
+            if raw > START:
+                out = raw - START
+            elif raw < START:
+                out = raw + (360 - START)
+            else:
+                out = 0
+            print(START, raw, ((out + 180) % 360) - 180)
 
+
+            '''
             print(f"Accelerometer [m/s^2]: x: {imuF.format(acceleroValues.x)} y: {imuF.format(acceleroValues.y)} z: {imuF.format(acceleroValues.z)}")
             print(f"Gyroscope [rad/s]: x: {imuF.format(gyroValues.x)} y: {imuF.format(gyroValues.y)} z: {imuF.format(gyroValues.z)} ")
-
+            '''
         if cv2.waitKey(1) == ord('q'):
             break
