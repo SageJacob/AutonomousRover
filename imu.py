c3514aca6409020aa546a7b4f32eb5c42d9adb37
Jacob Sage <46393418+SageJacob@users.noreply.github.com>
2022-04-22 13:28:46 -0400

    Update imu.py

--- imu.py
+++ imu.py
@@ -13,32 +13,36 @@ def setup():
     imu.out.link(xlinkOut.input)
     return pipeline
 
-START = None
-FLAG = 0
-curr = None
-prev = None
+def imu():
+    start = None
+    flag = 0
+    curr = None
+    prev = None
 
-with dai.Device(setup()) as device:
+    with dai.Device(setup()) as device:
 
-    imuQueue = device.getOutputQueue(name="imu", maxSize=50, blocking=False)
-    while True:
-        imuData = imuQueue.get()
-        imuPackets = imuData.packets
-        
-        for imuPacket in imuPackets:
-            rVvalues = imuPacket.rotationVector        
-            r = R.from_quat([rVvalues.real, rVvalues.i, rVvalues.j, rVvalues.k]) 
-            rotation = r.as_euler('zyz', degrees=True)
-            if FLAG == 0:
-                START = rotation[2] + 180
-                FLAG = 1
-                continue
-            raw = rotation[2] + 180
-            if raw > START:
-                out = raw - START
-            elif raw < START:
-                out = raw + (360 - START)
-            else:
-                out = 0
-            out = int(((out + 180) % 360) - 180)
-            print(out)
+        imuQueue = device.getOutputQueue(name="imu", maxSize=50, blocking=False)
+        while True:
+            imuData = imuQueue.get()
+            imuPackets = imuData.packets
+            
+            for imuPacket in imuPackets:
+                rVvalues = imuPacket.rotationVector        
+                r = R.from_quat([rVvalues.real, rVvalues.i, rVvalues.j, rVvalues.k]) 
+                rotation = r.as_euler('zyz', degrees=True)
+                if flag == 0:
+                    start = rotation[2] + 180
+                    flag = 1
+                    continue
+                raw = rotation[2] + 180
+                if raw > start:
+                    out = raw - start
+                elif raw < start:
+                    out = raw + (360 - start)
+                else:
+                    out = 0
+                out = int(((out + 180) % 360) - 180)
+                print(out)
+
+if __name__ == '__main__':
+    imu()
